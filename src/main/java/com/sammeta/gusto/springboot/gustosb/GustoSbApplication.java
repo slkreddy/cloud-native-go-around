633e3b73b67e3c0095fd9b0ff389c4493d4e157c
LaxmiKumar Reddy Sammeta <slkreddysite@gmail.com>
2021-01-04 15:22:27 +0530

    Initial commit of Gusto SB

---
+++ src/main/java/com/sammeta/gusto/springboot/gustosb/GustoSbApplication.java
@@ -0,0 +1,13 @@
+package com.sammeta.gusto.springboot.gustosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GustoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GustoSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sammeta/gusto/springboot/gustosb/GustoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sammeta.gusto.springboot.gustosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
