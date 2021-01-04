1dbbe62abea276cb92ca2e7645770aa7aadded29
LaxmiKumar Reddy Sammeta <slkreddysite@gmail.com>
2021-01-04 16:44:54 +0530

    First commit

--- src/main/java/com/sammeta/gusto/springboot/gustosb/GreetingController.java
+++ src/main/java/com/sammeta/gusto/springboot/gustosb/GreetingController.java
@@ -1,6 +1,5 @@
 package com.sammeta.gusto.springboot.gustosb;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,7 @@ public class GreetingController {
 
 	private static final String template = "Hellos, %s!";
 
-	@GetMapping("/greeting")
+	@GetMapping("/greeting" )
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format(template, name);
 	}
